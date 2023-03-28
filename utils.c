e3ec4c89a1f785682827582090083c09e188a8fd
Didiamaku <didiamaku@gmail.com>
2023-03-28 14:36:07 -0700

    utils.c

--- utils.c
+++ utils.c
@@ -2,7 +2,7 @@
 
 /**
  * is_printable - Evaluates if a char is printable or not
- * by Didi and Precious
+ * by Didi and Precious Ejims
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
